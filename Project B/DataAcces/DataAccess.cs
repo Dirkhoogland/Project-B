64a1f4c21ccfc494dcf7d5dd3300c2bd93e50a45
Dirkhoogland <99063413@mydavinci.nl>
2024-03-18 09:48:52 +0100

    Database creation and table creation
    sqlite database folder creation in the data access file

---
+++ Project B/DataAcces/DataAccess.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+namespace Project_B.DataAcces
+{
+    public class DataAccess
+    {
+        // string[] args
+        public static void Database()
+        {
+            SQLiteConnection sqlite_conn;
+            sqlite_conn = CreateConnection();
+            //CreateTable(sqlite_conn);
+            //InsertData(sqlite_conn);
+            ReadData(sqlite_conn);
+        }
+        private static string databasePath
+        {
+            get
+            {
+                return System.IO.Path.GetFullPath(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\DataSource"));
+            }
+        }
+        static SQLiteConnection CreateConnection()
+        {
+            SQLiteConnection sqlite_conn;
+            // Create a new database connection:
+            sqlite_conn = new SQLiteConnection($"Data Source={databasePath}\\database.db; Version = 3; New = True; Compress = True; ");
+            // Open the connection:
+            try { sqlite_conn.Open(); }
+            catch (Exception ex) { }
+            return sqlite_conn;
+        }
+
+        static void CreateTable(SQLiteConnection conn)
+        {
+
+            SQLiteCommand sqlite_cmd;
+            string Createsql = "CREATE TABLE Users("+
+                "ID INTEGER PRIMARY KEY AUTOINCREMENT,"+  
+                "Email VARCHAR(255),"+
+                "Name VARCHAR(225))";
+            sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd.CommandText = Createsql;
+            sqlite_cmd.ExecuteNonQuery();
+        }
+
+        static void InsertData(SQLiteConnection conn)
+        {
+            SQLiteCommand sqlite_cmd;
+            sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd.CommandText = "INSERT INTO Users(Email, Name) VALUES('Email ','Dirk'); ";
+            sqlite_cmd.ExecuteNonQuery();
+            sqlite_cmd.CommandText = "INSERT INTO Users(Email, Name) VALUES('Email1 ','Berat'); ";
+            sqlite_cmd.ExecuteNonQuery();
+            sqlite_cmd.CommandText = "INSERT INTO Users(Email, Name) VALUES('Email2 ','Mitchel'); ";
+            sqlite_cmd.ExecuteNonQuery();
+
+
+        }
+
+        static void ReadData(SQLiteConnection conn)
+        {
+            SQLiteDataReader sqlite_datareader;
+            SQLiteCommand sqlite_cmd;
+            sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd.CommandText = "SELECT * FROM Users";
+
+            sqlite_datareader = sqlite_cmd.ExecuteReader();
+            while (sqlite_datareader.Read())
+            {
+                string myreader = sqlite_datareader.GetString(0);
+                Console.WriteLine(myreader);
+            }
+            conn.Close();
+        }
+    }
+}
