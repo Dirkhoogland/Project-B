f42483a9fe2d762685741ad8c1b25b42d52ffdfa
Dirkhoogland <99063413@mydavinci.nl>
2024-03-20 11:10:47 +0100

    comments and usings
    Removed not needed using statements and added code comments to the registration and db functions

--- Project B/BusinessLogic/RegistrationLogic.cs
+++ Project B/BusinessLogic/RegistrationLogic.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Project_B.DataAcces;
+﻿using Project_B.DataAcces;
 
 namespace Project_B.BusinessLogic
 {
--- Project B/DataAcces/DataAccess.cs
+++ Project B/DataAcces/DataAccess.cs
@@ -1,34 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.SQLite;
-using System.Linq;
-using System.Reflection;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
+﻿using System.Data.SQLite;
 namespace Project_B.DataAcces
 {
     public class DataAccess
     {
-        // string[] args
+
         public static void Database()
         {
             CreateTable();
             InsertData();
             ReadData();
         }
+        // this function gets the path to the database for use in this application
         public static string databasePath
         {
             get
-            {
+            {   // gets the path to where ever its currently on your pc/laptop and then into a DataSource file, which if its correctly downloaded from github it should find.
                 return System.IO.Path.GetFullPath(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\DataSource"));
             }
         }
         static void CreateTable()
         {
-            // creates the user table with a ID, Email Password And name
+            // creates the user table with a ID, Email Name And Password, the ID is with an Primary key and Autoincrement.
             try
             {
                 string ConnectionString = $"Data Source={databasePath}\\database.db; Version = 3; New = True; Compress = True; ";
@@ -37,6 +29,7 @@ namespace Project_B.DataAcces
                     "Email VARCHAR(255)," +
                     "Name VARCHAR(255)," +
                     "Password VARCHAR(225))";
+                // using statements are used to confine the use of the connection to only this function, so the database remains useable outside of it since its automatially closed and does not remain open on a function when it shouldnt be
                 using (SQLiteConnection c = new SQLiteConnection(ConnectionString))
                 {
                     c.Open();
@@ -50,7 +43,8 @@ namespace Project_B.DataAcces
             catch (Exception ex) { }
 
         }
-
+        // the insert data function is a temple for inserting data into the sqlite DB, using the current Users database.
+        // the connectionstring will need a reference to the DataAccess file if used outside of it.
         static void InsertData()
         {
             string ConnectionString = $"Data Source={databasePath}\\database.db; Version = 3; New = True; Compress = True; ";
@@ -67,7 +61,7 @@ namespace Project_B.DataAcces
                         {
 
 
-
+                            // an sql query for inserting
                             sql = "INSERT INTO Users(Email, Name, Password) VALUES('Email','Dirk', 'Password');";
 
                              using (SQLiteCommand cmd1 = new SQLiteCommand(sql, c))
@@ -93,28 +87,26 @@ namespace Project_B.DataAcces
                     }
                 }
         }
-
+        // this function reads all data from the users table, its a template to use in other functions
         static void ReadData()
         {
             string ConnectionString = $"Data Source={databasePath}\\database.db; Version = 3; New = True; Compress = True; ";
-            string sql = "SELECT * FROM Users";
-            List<Users> Userslist = new List<Users>();
+            string sql = "SELECT * FROM Users"; 
             using (SQLiteConnection c = new SQLiteConnection(ConnectionString))
-            {
+            {   // opens the database connection
                 c.Open();
                 using (SQLiteCommand cmd = new SQLiteCommand(sql, c))
                 {
                     using (SQLiteDataReader rdr = cmd.ExecuteReader())
-                    {
+                    {   // this works as a for loop for each row in the database
                         while (rdr.Read())
-                        {
+                        {   // this part is to use the database data to put it into strings and ints to work in with c# there are premade functions for most datatypes
                             int Id = rdr.GetInt32(0);
                             string Email = rdr.GetString(1);
                             string Name = rdr.GetString(2);
                             string Password = rdr.GetString(3);
                             Console.WriteLine(Id + " "+ Email + " "+ Name + " "+ Password);
                         }
-
                     }
                 }
             }
