1c875f58f920648fb757b764e86f2db1a64e659e
Beratoshh <berat_kartal05@hotmail.com>
2024-03-25 13:55:52 +0100

    Update FlightsTest.cs

--- Project Btest/FlightsTest.cs
+++ Project Btest/FlightsTest.cs
@@ -1,32 +1,58 @@
 using System;
-using System.Collections.Generic;
-using System.Data;
 using System.Data.SQLite;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using Project_B.DataAcces;
-using Project_Btest;
-namespace Project_B.DataAcces.Flights
+
+namespace Project_Btest
 {
     [TestClass]
     public class FlightsTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        public static string databasePath
         {
-            // Arrange
-            var flights = new List<Flight>
-            {
-                new Flight { FlightNumber = "KL1234", Destination = "Amsterdam", Origin = "London", DepartureTime = DateTime.Now, Status = "On time" },
-                new Flight { FlightNumber = "KL1235", Destination = "Paris", Origin = "London", DepartureTime = DateTime.Now, Status = "On time" },
-                new Flight { FlightNumber = "KL1236", Destination = "Berlin", Origin = "London", DepartureTime = DateTime.Now, Status = "On time" }
-            };
-            // Act
-            var result = flights;
-            // Assert
-            Assert.AreEqual(3, result.Count);
+            get
+            {   // gets the path to where ever its currently on your pc/laptop and then into a DataSource file, which if its correctly downloaded from github it should find.
+                return System.IO.Path.GetFullPath(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\DataSource"));
+            }
         }
+        public void CreateFlightsTable()
+        {
+            try
+            {
+                string ConnectionString = $"Data Source={databasePath}\\database.db; Version = 3; New = True; Compress = True; ";
+                string[] sqlCommands = new string[]
+                {
+                    "CREATE TABLE IF NOT EXISTS Flights(" +
+                    "FlightID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "FlightNumber VARCHAR(255)," +
+                    "Destination VARCHAR(255)," +
+                    "Origin VARCHAR(255)," +
+                    "DepartureTime DATETIME," +
+                    "Status VARCHAR(255)," +
+                    "Terminal VARCHAR(255)," +
+                    "AircraftType VARCHAR(255)," +
+                    "Gate VARCHAR(255)," +
+                    "Seats INTEGER," +
+                    "AvailableSeats INTEGER," +
+                    "Airline VARCHAR(255))",
+                    "ALTER TABLE Flights ADD COLUMN Origin VARCHAR(255)",
+                    "ALTER TABLE Flights ADD COLUMN Status VARCHAR(255)",
+                    "ALTER TABLE Flights ADD COLUMN Gate VARCHAR(255)",
+                    "ALTER TABLE Flights ADD COLUMN AvailableSeats INTEGER"
+                };
+
+                using (SQLiteConnection c = new SQLiteConnection(ConnectionString))
+                {
+                    c.Open();
+                    foreach (var sql in sqlCommands)
+                    {
+                        using (SQLiteCommand cmd = new SQLiteCommand(sql, c))
+                        {
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { }
+        }
+
     }
 }
