bbc2575b7e4ab3a12e0a01ea404390146b00c2b6
Dirkhoogland <99063413@mydavinci.nl>
2024-04-03 14:38:46 +0200

    check program.cs

--- Project B/Program.cs
+++ Project B/Program.cs
@@ -12,7 +12,11 @@ namespace Project_B
 
             DataAccess.Database();
             CurrentUser currentuser = Login();
-            Console.WriteLine($"Name: {currentuser.Name} Logged in: {currentuser.LoggedIn}");
+            try
+            {
+                Console.WriteLine($"Name: {currentuser.Name} Logged in: {currentuser.LoggedIn}");
+            }
+            catch(Exception ex) { }
             //lay_out lay_out = new lay_out();
             //lay_out.ToonMenu();
             // Verwijder de CreateFlightsTable() als je niet Database wilt aanmaken, dan kan ook 3 eronder niet gebruikt worden. Verwijder changes als je database hebt aangemaakt ivm bugs.
