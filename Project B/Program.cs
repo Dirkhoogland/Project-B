bfd65a3208e22ea8c7b845b22ec36358c9bd7306
Beratoshh <berat_kartal05@hotmail.com>
2024-04-15 19:30:47 +0200

    Update Program.cs

--- Project B/Program.cs
+++ Project B/Program.cs
@@ -11,7 +11,6 @@ namespace Project_B
         {
 
 
-            // Verwijder de CreateFlightsTable() als je niet Database wilt aanmaken, dan kan ook 3 eronder niet gebruikt worden. Verwijder changes als je database hebt aangemaakt ivm bugs.
 
             DataAccess.Database();
             CurrentUser currentuser = Login();
@@ -23,7 +22,6 @@ namespace Project_B
             Seat seat = new Seat();
             seat.lay_out();
             seat.ToonMenu();
-            // Verwijder de CreateFlightsTable() als je niet Database wilt aanmaken, dan kan ook 3 eronder niet gebruikt worden. Verwijder changes als je database hebt aangemaakt ivm bugs.          
             //lay_out lay_out = new lay_out();
             //lay_out.ToonMenu();
 
