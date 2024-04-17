429f3f241017f9d155804a97585af46c0acbb5cd
Beratoshh <berat_kartal05@hotmail.com>
2024-04-17 21:01:25 +0200

    menu toegevoegd bij login

--- Project B/Presentation/LoginRegistration.cs
+++ Project B/Presentation/LoginRegistration.cs
@@ -9,17 +9,69 @@ namespace Project_B.Presentation
 
         // checks if the user wants to log in or register 
         public static CurrentUser LoginScreen()
-        {   bool check = false;
+        {
+            bool check = false;
             CurrentUser currentUser = null;
-            while (check is false)
-            {   
-                Console.WriteLine("Wil je in loggen of Registreren \n Login/Register/Guest");
-                string? inputunchecked = Console.ReadLine();
-                string input = inputunchecked.ToLower();
-                if (input == "login") { check = true; currentUser = Loginscreen(); }
-                else if (input == "register") { check = true; currentUser = Registrationscreen(); }
-                else if (input == "guest") { check = true; currentUser = Guestscreen(); }
+            string[] loginOptions = { "Login", "Register", "Guest" };
+            int currentIndex = 0;
+
+            while (!check)
+            {
+                Console.Clear();
+
+                for (int i = 0; i < loginOptions.Length; i++)
+                {
+                    if (i == currentIndex)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Gray;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                    }
+
+                    Console.WriteLine(loginOptions[i]);
+
+                    Console.ResetColor();
+                }
+
+                ConsoleKeyInfo keyInfo;
+                do
+                {
+                    keyInfo = Console.ReadKey(true);
+                } while (keyInfo.Key != ConsoleKey.UpArrow && keyInfo.Key != ConsoleKey.DownArrow && keyInfo.Key != ConsoleKey.Enter);
+
+                if (keyInfo.Key == ConsoleKey.UpArrow)
+                {
+                    if (currentIndex > 0)
+                    {
+                        currentIndex--;
+                    }
+                }
+                else if (keyInfo.Key == ConsoleKey.DownArrow)
+                {
+                    if (currentIndex < loginOptions.Length - 1)
+                    {
+                        currentIndex++;
+                    }
+                }
+                else if (keyInfo.Key == ConsoleKey.Enter)
+                {
+                    switch (loginOptions[currentIndex])
+                    {
+                        case "Login":
+                            check = true;
+                            currentUser = Loginscreen();
+                            break;
+                        case "Register":
+                            check = true;
+                            currentUser = Registrationscreen();
+                            break;
+                        case "Guest":
+                            check = true;
+                            currentUser = Guestscreen();
+                            break;
+                    }
+                }
             }
+
             return currentUser;
         }
         // registration asks for user details to then send to the business logic side
@@ -31,11 +83,12 @@ namespace Project_B.Presentation
             // Asks users email and password for registration, the strings are nullable so the function/responses will loop if not both filled in.
             while (check is false)
             {
-                Console.WriteLine("Please fill in your Email");
-                Email = Console.ReadLine();
-                Console.WriteLine("Please fill in your Name");
-                Name = Console.ReadLine();
-                Console.WriteLine("Please fill in your Password");
+                Console.WriteLine("Please fill in your email:");
+                Email = Console.ReadLine().ToLower();
+                Console.WriteLine("Please fill in your first name:");
+                string nameInput = Console.ReadLine();
+                Name = char.ToUpper(nameInput[0]) + nameInput.Substring(1).ToLower();
+                Console.WriteLine("Please fill in your password:");
                 Password = Console.ReadLine();
                 if (Email != null && Password != null && Name != null) { check = true; }
             }
