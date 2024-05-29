a86360755ac90ba7679d625c540251b1bb236d1a
badr <1078106@hr.nl>
2024-05-29 15:03:00 +0200

    Fixed and finished unittests

--- Project Btest/JSONTests.cs
+++ Project Btest/JSONTests.cs
@@ -6,9 +6,11 @@ namespace Project_Btest
     public class JSONconversionTests
     {
         [TestMethod]
-        public void JSONconversionTest()
+
+        // Check if the created JSON file doesn't return empty
+        public void JSONfileTest()
         {
-            string relativeDatabasePath = Path.Combine("..", "..", "..", "Project B", "DataSource");
+            string relativeDatabasePath = Path.Combine("..", "..", "..", "..", "Project B", "DataSource");
             string absoluteDatabasePath = Path.GetFullPath(relativeDatabasePath);
             string databaseFileName = "database.db";
             string databaseFilePath = Path.Combine(absoluteDatabasePath, databaseFileName);
@@ -19,6 +21,19 @@ namespace Project_Btest
             string ConnectionString = $"Data Source={databaseFilePath}; Version = 3;";
             List<JSONconversion> tickets = JSONconversion.Convert_to_Json(ConnectionString);
             Assert.IsNotNull(tickets);
+        }
+
+        [TestMethod]
+
+        //Check if JSON file is located in the "Documents" folder.
+        public void JSONlocationTest()
+        {
+            string relativeJsonPath = Path.Combine("..", "..", "..", "..", "..", "..", "Documents");
+            string absoluteJsonPath = Path.GetFullPath(relativeJsonPath);
+            string JsonFileName = "NewSouthTicketData.json";
+            string JsonFilePath = Path.Combine(absoluteJsonPath, JsonFileName);
+
+            Assert.IsTrue(File.Exists(JsonFilePath), $"JSON file not found at {JsonFilePath}");
         }
     }
 }
