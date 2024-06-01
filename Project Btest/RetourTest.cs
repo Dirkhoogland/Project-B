5980ff78ca2d70c19d673e982e66e0f01036a16f
Young $heldon <1078106@hr.nl>
2024-06-01 02:36:29 +0200

    Added retour testing format

---
+++ Project Btest/RetourTest.cs
@@ -0,0 +1,22 @@
+namespace Project_Btest
+{
+    [TestClass]
+    public class RetourTest
+    {
+        [TestMethod]
+        public void TestRetourReservation()
+        {
+
+        }
+
+        public void TestRetourWithBaggage()
+        {
+
+        }
+
+        public void TestRetourCancellation()
+        {
+            
+        }
+    }
+}
